cadb048f20d5b9f380288500907589ba832d9c09
aravindhkumaran <aravindh1097@gmail.com>
2024-09-08 21:04:47 -0400

    added dbt-expectations & update increment strategy

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -19,6 +19,19 @@ FROM
     src_reviews
 WHERE 
     review_text IS NOT NULL
+
+
 {% if is_incremental() %}
-    AND review_date > ( SELECT MAX(review_date) FROM {{ this }} )
+    {% if var("start_date", False) and var("end_date", False) %}
+        {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+        AND review_date >= '{{ var("start_date") }}'
+        AND review_date < '{{ var("end_date") }}'
+    {% else %}
+        AND review_date > ( SELECT MAX(review_date) FROM {{ this }} )
+        {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True) }}
+    {% endif %}
 {% endif %}
+
+
+-- pass start_date and end_date in terminal using this command
+-- dbt run -s fct_reviews --vars "{start_date: 2024-02-15 00:00:00, end_date: 2024-03-15 23:59:59}"
